9e67be53fb416d58cc904323b4e3fa09fea1c153
u-a-b <uamayabanegas26@aoiths.org>
2025-11-18 13:39:53 -0500

    progress1

---
+++ script.js
@@ -0,0 +1,50 @@
+
+let submitButton = document.querySelector(".btn btn-primary fw-semibold");
+let resetButton = document.querySelector(".btn btn-outline-secondary");
+
+
+
+
+
+
+
+
+
+
+// CREATE FUNCTIONS W/ LOOPS
+function countToFifteen(){
+    for(let x = 1; x <= 15; x++){
+        console.log(x);
+    }
+}
+
+countToFifteen();
+
+function countFifteenDown(){
+     for(let x = 15; x >= 1; x--){
+        console.log(x);
+    }
+}
+
+countFifteenDown();
+
+function countToThirty(){
+    for(let x = 1; x <= 30; x++){
+        console.log(x);
+    }
+}
+
+function multiplesOfFive(){
+    for(let x = 30; x <= 35; x++){
+        console.log(x);
+    }
+}
+
+
+function fullRange(){
+    for(let x = -50; x <= 50; x++){
+        console.log(x);
+    }
+}
+
+fullRange();
